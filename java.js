954978dbdaf7e58e625e9341aa286503b96bcc25
aditya359 <chaubeyaditya2004@gmail.com>
2022-08-15 12:05:46 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,34 @@
+script
+console.log(2+2)
+function myfunction()
+{
+let c
+var a = document.getElementById("input1").value;
+var b = document.getElementById("input2").value;
+c =  a/b;
+document.getElementById('d').innerHTML=c
+}
+function myfunction2()
+{
+let c
+var a = document.getElementById("input1").value;
+var b = document.getElementById("input2").value;
+c =  a*b;
+document.getElementById('d').innerHTML=c
+}
+function myfunction3()
+{
+let c
+var a = document.getElementById("input1").value;
+var b = document.getElementById("input2").value;
+c =  a-b;
+document.getElementById('d').innerHTML=c
+}
+function myfunction4()
+{
+const a = parseInt(document.getElementById("input1").value);
+const b = parseInt(document.getElementById("input2").value);
+const c =  a + b;
+document.getElementById('d').innerHTML=c;
+}
+script
